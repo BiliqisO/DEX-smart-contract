00efc2741cd332d7f307d2712c7f37f67cc0c25b
BiliqisO <biliqisonikoyi@gmail.com>
2023-09-11 15:25:11 +0100

    assignment update

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -3,26 +3,25 @@ pragma solidity ^0.8.9;
 
 import "./interface.sol";
 
+
+
 contract Swap{
     ISwap tokenA;
     ISwap tokenB;
-    uint ReserveA;
-    uint ReserveB;
-    address user;
-    uint liquidity;
-    uint k;
+     uint  ReserveA;
+    uint  ReserveB;
     struct LiquidityProvider{
         uint AmountA;
         uint AmountB;
     }
-mapping (address => LiquidityProvider) mapLiquidityProvider;
-mapping (address => uint) public Liquidity;
+mapping (address => LiquidityProvider) public  mapLiquidityProvider;
     constructor (address _TokenA, address _TokenB) {
         tokenA = ISwap(_TokenA);
         tokenB = ISwap(_TokenB);
         // user = msg.sender;
     }
-   function addLiquidity(uint _amountA, uint _amountB) external {
+ //add liquidity into the contract
+    function addLiquidity(uint _amountA, uint _amountB) external {
         tokenA.transferFrom(msg.sender, address(this), _amountA);
         tokenB.transferFrom(msg.sender, address(this), _amountB);
         ReserveA += _amountA;
@@ -30,44 +29,50 @@ mapping (address => uint) public Liquidity;
       LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
       liquidityProvider.AmountA += _amountA;
       liquidityProvider.AmountB += _amountB;
-      Liquidity[msg.sender] =  _amountA * _amountB;
-      k =  Liquidity[msg.sender];
-      user = msg.sender;
-
+      
     }
-   function removeLiquidity(uint _amountA, uint _amountB) external {
-  require (user == msg.sender, "you are not authorised to remove liquisity");
-        tokenA.transfer(user, _amountA);
-        tokenB.transfer(user, _amountB);
-        ReserveA -= _amountA;
-        ReserveB -= _amountB;
+
+
+    function removeLiquidity(uint _amountA, uint _amountB) external {
       LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
+      require(liquidityProvider.AmountA >= _amountA, "Insuffiecient Token A Balance for provider");
+      require(liquidityProvider.AmountB >= _amountB, "Insuffiecient Token B Balance for provider");
+
       liquidityProvider.AmountA -= _amountA;
       liquidityProvider.AmountB -= _amountB;
-        Liquidity[user] =  _amountA * _amountB;
-      k =  Liquidity[user];
+      ReserveA -= _amountA;
+      ReserveB -= _amountB;
+      
+      tokenA.transfer(msg.sender, _amountA);
+      tokenB.transfer(msg.sender, _amountB);
+    }
 
+
+    function K() public view returns(uint){
+      return   ReserveA * ReserveB;
     }
-    function swapAforB(uint amounta, address receiver) external{
-         LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
-         uint amountA= liquidityProvider.AmountA;
-         uint amountB = liquidityProvider.AmountB;
-        uint amountb = amountB - (k / (amountA + amounta));
-        tokenA.transfer( receiver, amountb);
-        mapLiquidityProvider[msg.sender].AmountA -= amounta;
-        mapLiquidityProvider[receiver].AmountB += amountb;
+
+    
+    function swapAforB(uint amounta) external{
+      require(tokenA.balanceOf(msg.sender) >= amounta, "Insufficient funds ");
+       uint amountb = ReserveB - (K() / (ReserveA + amounta));
+ 
+      tokenA.transferFrom(msg.sender, address(this), amounta);  
+      LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
+      liquidityProvider.AmountA += amounta;
+      liquidityProvider.AmountB -= amountb;
+       
+      tokenB.transfer(msg.sender, amountb);
     }
-    function swapBforA(uint  amountb, address receiver) external{
-         LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
-         uint amountA = liquidityProvider.AmountA;
-          uint amountB = liquidityProvider.AmountB;
-        uint amounta = amountA - (k / (amountB - amountb));
-         tokenB.transfer(receiver, amounta);
-          mapLiquidityProvider[msg.sender].AmountB -= amountb;
-               mapLiquidityProvider[receiver].AmountA += amounta;
+    function swapBforA(uint  amountb) external{
+      require(tokenB.balanceOf(msg.sender) >= amountb, "Insufficient funds ");
+       uint amounta = ReserveA - (K() / (ReserveB + amountb));
+  LiquidityProvider storage liquidityProvider =  mapLiquidityProvider[msg.sender];
+      tokenB.transferFrom(msg.sender, address(this), amounta);  
+       liquidityProvider.AmountA += amountb;
+      liquidityProvider.AmountB  -= amounta;
+
+      tokenA.transfer(msg.sender, amountb);
     }
-    function returnvalues( address _address) external view returns (uint){
-  return(  Liquidity[_address] );
-    }
+
 }
-
--- scripts/swap.ts
+++ scripts/swap.ts
@@ -1,31 +1,40 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [account1, account2, account3] = await ethers.getSigners();
+  const [account1, account2] = await ethers.getSigners();
   const TokenA = "0xcEA38fac44aeb835b6b8719dE84FB471471b264f";
   const TokenB = "0x06d1E46ca659936f8A85522D3bf7A72AAd8D187b";
-
   const addresses = [TokenA, TokenB];
   const swap = await ethers.deployContract("Swap", addresses);
-
   await swap.waitForDeployment();
   console.log(`deployed to ${swap.target}`);
-  const interactWithswap = await ethers.getContractAt("Swap", swap.target);
+  const swapTarget = swap.target;
+  const interactWithswap = await ethers.getContractAt("Swap", swapTarget);
   const interactWithTokenA = await ethers.getContractAt("IERC20", TokenA);
   const interactWithTokenB = await ethers.getContractAt("IERC20", TokenB);
-  await interactWithTokenA.transfer(account2, 5000);
-  await interactWithTokenB.transfer(account2, 8000);
+
+  await interactWithTokenA.transfer(account2, ethers.parseUnits("90000"));
+  await interactWithTokenB.transfer(account2, ethers.parseUnits("80000"));
 
   // user1 approves swapping contract
-  await interactWithTokenA.connect(account2).approve(swap.target, 5000);
-  await interactWithTokenB.connect(account2).approve(swap.target, 8000);
+  await interactWithTokenA
+    .connect(account2)
+    .approve(swapTarget, ethers.parseUnits("90000"));
+  await interactWithTokenB
+    .connect(account2)
+    .approve(swapTarget, ethers.parseUnits("80000"));
   //user1 put in 3000 token1
-  await interactWithswap.addLiquidity(200, 400);
+  await interactWithswap
+    .connect(account2)
+    .addLiquidity(ethers.parseUnits("200"), ethers.parseUnits("500"));
   const balanceofUser1A = await interactWithTokenA.balanceOf(account2);
   const balanceofUser1B = await interactWithTokenB.balanceOf(account2);
   console.log(`balanceofUser1A :${balanceofUser1A}`);
   console.log(`balanceofUser1B :${balanceofUser1B}`);
-  await interactWithswap.swapAforB(ethers.parseUnits("150"), account3.address);
+  // await interactWithswap
+  //   .connect(buyer)
+  //   .userGetTokenAorB(ethers.parseUnits("0"), ethers.parseUnits("400"));
+  await interactWithswap.connect(account2).swapAforB(ethers.parseUnits("50"));
   const balanceofUser1AfterSwap = await interactWithTokenA.balanceOf(account2);
   const balanceofUser1BAfterSwap = await interactWithTokenB.balanceOf(account2);
   console.log(`balanceofUser1A :${balanceofUser1AfterSwap}`);
